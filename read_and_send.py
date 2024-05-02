449e2058654aeec4569964c787f339744a267d66
cherybloo <laurenciomichael98@gmail.com>
2024-05-02 17:05:47 +0200

    Add user input for flexibility

--- read_and_send.py
+++ read_and_send.py
@@ -1,6 +1,16 @@
 import os.path
-import json
 import base64
+import email
+import re
+import sys
+
+import mimetypes
+from email.message import EmailMessage
+from email.mime.audio import MIMEAudio
+from email.mime.base import MIMEBase
+from email.mime.image import MIMEImage
+from email.mime.text import MIMEText
+from email.mime.multipart import MIMEMultipart
 
 from google.auth.transport.requests import Request
 from google.oauth2.credentials import Credentials
@@ -15,6 +25,20 @@ from googleapiclient.errors import HttpError
 # Full access to the account's mailboxes
 SCOPES = ["https://mail.google.com/"]
 
+def main():
+	user_email = input("Enter your email address: ")
+	maxNumbers = input("Enter the maximum numbers: ")
+	query = input("Query: ")
+
+	service = gmail_authentication()
+	get_messages_id(service,query,maxNumbers, user_email) 
+	# service -> what kind of service do you want to retrieve
+	# query -> (string) that you want to search from the mailboxes
+	# maxNumbers -> (integer) maximal number of messages that you want to retrieve from your mailboxes
+
+# get id -> get message -> get recipients -> send the email text
+
+# Handle the GMAIL authentication
 def gmail_authentication():
 	creds = None
 
@@ -32,26 +56,58 @@ def gmail_authentication():
 		# Save the credentials for the next run
 		with open("token.json", "w") as token:
 			token.write(creds.to_json())
-	
-	# Return all the services
+	# Return service
 	return build("gmail", "v1", credentials=creds)
 
-service = gmail_authentication()
+def send_message(service, recipients, user_email):
+    for target_email in recipients:
+        try:
+            message = MIMEMultipart()
+            message['To'] = target_email
+            message['From'] = user_email
+            message['Subject'] = 'Bruh'
+            html_content = "<div dir=\"ltr\"><img src=\"https://media1.tenor.com/m/_TemusfqiUMAAAAC/unbelievable-the-office.gif\" width=\"452\" height=\"252\"><br></div>"
+            html_part = MIMEText(html_content, 'html')
+            message.attach(html_part)
 
-# service -> what kind of service do you want to retrieve
-# query -> (string) that you want to search from the mailboxes
-# maxNumbers -> (integer) maximal number of messages that you want to retrieve from your mailboxes
-def get_messages_body(service, message_id):
-	messages = []
-	for messageId in message_id: # Iterate through the message Id
-		message = service.users().messages().get(
-			userId = "me",
-			id = messageId,
-			format = "raw"
-		).execute()
-		print(message["snippet"],end="\n\n")
+            # encoded message
+            encoded_message = base64.urlsafe_b64encode(message.as_bytes()).decode()
 
-def get_messages_id(service, query, maxNumbers):
+            create_message = {"raw": encoded_message}
+            send_message = (
+                service.users()
+                .messages()
+                .send(userId="me", body=create_message)
+                .execute()
+            )
+            print(f'Message Id: {send_message["id"]}')
+        except HttpError as error:
+            print(f"An error occurred: {error}")
+            send_message = None
+
+def get_messages_body(service, message_id, user_email):
+	recipients = []
+	try:
+		for messageId in message_id: # Iterate through the message Id
+			message = service.users().messages().get(
+				userId = "me",
+				id = messageId,
+				format = "raw"
+			).execute()
+			mime_msg = email.message_from_bytes(base64.urlsafe_b64decode(message["raw"]))
+
+			msg_from = mime_msg['from']
+			msg_to = mime_msg['to']
+			msg_subject = mime_msg['subject']
+			pattern = r"\b[A-Za-z0-9._%+-]+@[A-Za-z0-9.-]+\.[A-Z|a-z]{2,}\b"
+			recipient = re.findall(pattern,msg_from)[0]
+			if recipient not in recipients and recipient != user_email:
+				recipients.append(recipient)
+		send_message(service, recipients, user_email)
+	except HttpError as error:
+		print(f"An error occurred: {error}")
+
+def get_messages_id(service, query, maxNumbers, user_email):
 	# Call the API
 	try:
 		messages = service.users().messages().list(
@@ -60,22 +116,17 @@ def get_messages_id(service, query, maxNumbers):
 			q = query,
 		).execute()
 		messages_response = messages.get("messages", [])
-
 		if not messages_response:
 			print("The query doesn't match any messages")
 			return
-		
 		# print(messages_response)
 		message_id = []
 		for id in messages_response:
 			message_id.append(id['id'])
-		#print(message_id)
-		get_messages_body(service, message_id)
-		#return message_id
-
+		print(message_id)
+		get_messages_body(service, message_id, user_email)
 	except HttpError as error:
-		# TODO(developer) - Handle errors from gmail API.
 		print(f"An error occurred: {error}")
 
-
-get_messages_id(service,"leider",2)
+if __name__ == "__main__":
+	main()
